a755ffc517856c5e63a0b4dec04e008c0e15a9fc
shiro <67919489+UnleashedFurai@users.noreply.github.com>
2026-03-31 12:46:57 -0500

    emacs: trying exwm

--- .bashrc
+++ .bashrc
@@ -1,3 +1,9 @@
 alias rm=trash
 alias s=sudo
 alias sedit=sudoedit
+
+# alias emacs="emacsclient -t"
+
+if [[ -z $DISPLAY && "$(tty 2>/dev/null)" == "/dev/tty1" ]]; then
+	exec startx
+fi
--- emacs/init.el
+++ emacs/init.el
@@ -14,28 +14,14 @@
 (use-package emacs
   :init
   ;; set window size
-  (if (window-system)
-      (set-frame-size (selected-frame) 90 40))
+  ;; (if (window-system)
+  ;;    (set-frame-size (selected-frame) 90 40))
 
   ;; disable splash screen / menu bars
   (setq inhibit-startup-screen t)
   (menu-bar-mode 0)
   (tool-bar-mode 0)
   (scroll-bar-mode 0)
-
-  ;; highlight current line
-  (global-hl-line-mode 1)
-
-  ;; relative line numbers
-  (setq display-line-numbers-type 'relative)
-  (global-display-line-numbers-mode 1)
-
-  ;; fill column indicator at 80
-  (setq-default fill-column 80)
-  (global-display-fill-column-indicator-mode 1)
-
-  ;; follow mode
-  (follow-mode 1)
 
   ;; disable cursor blink
   (blink-cursor-mode 0)
@@ -52,7 +38,34 @@
 			   (expand-file-name "themes" user-emacs-directory))
   (load-theme 'quiet t))
 
-;; evil mode
+(use-package hl-line
+  :ensure nil
+  :defer t
+  :hook ((prog-mode . hl-line-mode)
+		 (org-mode . hl-line-mode)
+		 (markdown-mode . hl-line-mode)
+		 (latex-mode . hl-line-mode)))
+
+(use-package display-line-numbers
+  :ensure nil
+  :defer t
+  :config
+  (setq display-line-numbers-type 'relative)
+  :hook ((prog-mode . display-line-numbers-mode)
+		 (org-mode . display-line-numbers-mode)
+		 (markdown-mode . display-line-numbers-mode)
+		 (latex-mode . display-line-numbers-mode)))
+
+(use-package display-fill-column-indicator
+  :ensure nil
+  :defer t
+  :config
+  (setq-default fill-column 80)
+  :hook ((prog-mode . display-fill-column-indicator-mode)
+		 (org-mode . display-fill-column-indicator-mode)
+		 (markdown-mode . display-fill-column-indicator-mode)
+		 (latex-mode . display-fill-column-indicator-mode)))
+
 (use-package evil
   :init
   (setq evil-want-integration t
@@ -79,7 +92,6 @@
   :after evil
   :hook (prog-mode . hs-minor-mode))
 
-;; smex
 (use-package smex
   :bind (("M-x" . smex)
          ("M-X" . smex-major-mode-commands)
@@ -149,7 +161,7 @@
   :config
   (setq-default c-basic-offset 4))
 
-(use-package term
+(use-package vterm
   :config
   (defun term-other-window ()
 	"Open term in the other window."
@@ -162,5 +174,72 @@
 						(split-window-below))
 					(next-window)))
 	  (select-window other)
-	  (term shell)))
+	  (vterm shell)))
   (define-key ctl-x-4-map (kbd "t") 'term-other-window))
+
+(use-package exwm
+  :config
+  ;; (setq exwm-workspace-number 4)
+  (setq exwm-input-global-keys
+		(append exwm-input-global-keys
+   				`(
+   				  (,(kbd "s-h") . windmove-left)
+   				  (,(kbd "s-j") . windmove-down)
+   				  (,(kbd "s-k") . windmove-up)
+   				  (,(kbd "s-l") . windmove-right)
+   				  (,(kbd "s-b") . switch-to-buffer)
+				  (,(kbd "s-<escape>") . exwm-reset))))
+  (dotimes (i 10)
+	(exwm-input-set-key (kbd (format "s-%d" (if (= i 9) 0 (1+ i))))
+						`(lambda ()
+						   (interactive)
+						   (exwm-workspace-switch-create ,i))))
+  ;; default x applications to char-mode
+  ;; (setq exwm-manage-configurations '((t char-mode t)))
+  
+  ;; update buffer titles of x windows
+  (defun exwm-rename-buffer ()
+	(interactive)
+	(exwm-workspace-rename-buffer
+	 (concat exwm-class-name ":"
+			 (if (<= (length exwm-title) 50) exwm-title
+               (concat (substring exwm-title 0 49) "...")))))
+  (add-hook 'exwm-update-class-hook 'exwm-rename-buffer)
+  (add-hook 'exwm-update-title-hook 'exwm-rename-buffer)
+
+  (defun async-shell-hidden-buffer (command)
+	"Run async shell command without a window for the buffer."
+	(interactive (list
+				  (read-shell-command "Async shell command: ")))
+	(let ((display-buffer-alist
+		   '(("\\*Async Shell Command\\*"
+			  (display-buffer-no-window)))))
+	  (async-shell-command command)))
+  (global-set-key (kbd "M-&") #'async-shell-hidden-buffer))
+
+(use-package openwith
+  :config
+  ;; throws some win32 compile error
+  (setq native-comp-async-report-warnings-errors nil)
+  (setq openwith-associations
+		(list
+		 (list (openwith-make-extension-regexp
+				'("pdf"
+				  "docx" "xlsx" "pptx"
+				  "doc" "xls" "ppt"
+				  "odt" "ods" "odp"))
+			   "zaread"
+			   '(file))
+		 (list (openwith-make-extension-regexp
+				'("mkv" "mp4" "avi"))
+			   "mpv"
+			   '(file))
+		 (list (openwith-make-extension-regexp
+				'("mp3" "opus" "flac" "wav" "aac"))
+			   "mpv"
+			   '(file))
+		 (list (openwith-make-extension-regexp
+				'("jpg" "jpeg" "jxl" "png" "webp" "gif" "avif"))
+			   "mpv"
+			   '(file))))
+  (openwith-mode 1))
